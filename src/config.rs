e6982a8d361d70e75baf6766953357ebef9e4b3b
Raphaël Thériault <raphael_theriault@outlook.com>
2020-03-10 22:23:02 -0400

    Config

--- src/config.rs
+++ src/config.rs
@@ -1,7 +1,24 @@
 use once_cell::sync::Lazy;
+use serde::{Deserialize, Serialize};
 use std::{env, path::PathBuf};
 
 pub static MECA_HOME: Lazy<PathBuf> = Lazy::new(|| match env::var_os("MECA_HOME") {
     Some(v) => PathBuf::from(v),
     None => dirs::home_dir().unwrap().join(".meca"),
 });
+
+#[derive(Deserialize, Serialize)]
+#[serde(default)]
+pub struct Config {
+    resources_url: String,
+}
+
+impl Default for Config {
+    fn default() -> Self {
+        Self {
+            resources_url:
+                "https://raw.githubusercontent.com/raftario/Meca/master/resources/metadata.json"
+                    .to_owned(),
+        }
+    }
+}
